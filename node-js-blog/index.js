23a95d49f3fd808f2a904325d180f17713e52a95
Tatsuro <contacts@tatsuro.dev>
2020-10-16 19:40:16 +0900

    template engineのedge install, setup 14

--- node-js-blog/index.js
+++ node-js-blog/index.js
@@ -1,11 +1,17 @@
 const path = require('path')
+// 分割代入で別名保存
+const { engine: expressEdge } = require('express-edge')
 const express = require('express')
 
 const app = new express()
 app.use(express.static('public'))
+app.use(expressEdge)
+// templateのpathを指定
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'index.html'))
+  // renderでtemplateを表示
+  res.render('index')
 })
 
 app.get('/about', (req, res) => {
