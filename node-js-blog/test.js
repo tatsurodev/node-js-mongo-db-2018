6d4e8927e3a73a1d143301f1aaca10a049d81084
Tatsuro <contacts@tatsuro.dev>
2020-10-17 06:04:11 +0900

    documents read 20

--- node-js-blog/test.js
+++ node-js-blog/test.js
@@ -3,6 +3,7 @@ const Post = require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost/node-js-test-blog')
 
+/* create
 // documentの作成、非同期的処理なのでcallbackを第二引数に指定可
 Post.create({
   title: 'My first blog post',
@@ -10,4 +11,13 @@ Post.create({
   content: 'Lorem ipsum content',
 }, (error, post) => {
   console.log(error, post)
+})
+*/
+
+// read
+// findの第一引数でdocument検索、空objectの指定で全該当documents取得
+Post.find({
+  title: 'My first blog post',
+}, (error, posts) => {
+  console.log(error, posts)
 })
