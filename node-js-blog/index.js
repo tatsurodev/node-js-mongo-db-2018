ecfddc255b399fcb8e343d5fcfdef99bc74faefb
Tatsuro <contacts@tatsuro.dev>
2020-10-16 17:28:41 +0900

    about, post, contact pageを移植 13

--- node-js-blog/index.js
+++ node-js-blog/index.js
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'pages', 'index.html'))
 })
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'about.html'))
+})
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'post.html'))
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'contact.html'))
+})
+
 app.listen(4000, () => {
   console.log('App listening on port 4000')
 })
