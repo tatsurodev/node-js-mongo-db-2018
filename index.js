ca55edbba66bdce11daa058479ab3785273c589a
Tatsuro <contacts@tatsuro.dev>
2020-10-16 13:34:53 +0900

    urlによって返すresponseを分岐 4

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-  console.log(request.url)
-  response.end('HELLO NODE JS')
+  if (request.url === '/about') {
+    // returnを使うことで条件合致時にこの条件分岐を終了させる, response.endでstatus code 200が返る
+    return response.end('THE ABOUT PAGE')
+  } else if (request.url === '/contact') {
+    return response.end('THE CONTACT PAGE')
+  } else if (request.url === '/') {
+    return response.end('THE HOME PAGE')
+  } else {
+    response.writeHead(404)
+    response.end('THE PAGE WAS NOT FOUND.')
+  }
 })
 
 // node index.jsでserver起動、localhost:3000でaccess可
