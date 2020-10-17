426d326944e99dfd834808460e221e887d2767f9
Tatsuro <contacts@tatsuro.dev>
2020-10-17 19:24:31 +0900

    loginUserControllerのlogin処理outline作成 43

--- node-js-blog/controllers/loginUser.js
+++ node-js-blog/controllers/loginUser.js
@@ -1,5 +1,26 @@
+const bcrypt = require('bcrypt')
 const User = require('../database/models/User')
 
 module.exports = (req, res) => {
-  res.redirect('/')
+  const { email, password } = req.body
+  // try to find
+  User.findOne({
+    email,
+  }, (error, user) => {
+    if (user) {
+      // formのpasswordとUser modelに保存されているpasswordが一緒かcheck, callbackの第二引数sameは一致したかどうかのbooleanが格納
+      bcrypt.compare(password, user.password, (error, same) => {
+        // passwordが一致
+        if (same) {
+          // sessionを保存
+          res.redirect('/')
+          // password不一致
+        } else {
+          res.redirect('/auth/login')
+        }
+      })
+    } else {
+      return res.redirect('/auth/login')
+    }
+  })
 }
