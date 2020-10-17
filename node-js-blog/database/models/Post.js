891e88b17448ca56748b6cc0b9d5d9e80eb65ace
Tatsuro <contacts@tatsuro.dev>
2020-10-17 10:12:12 +0900

    Post modelのdescription fieldをsubtitleにrename 30

--- node-js-blog/database/models/Post.js
+++ node-js-blog/database/models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 // schemaを定義し、collectionの中のdocumentの構造を明記
 const PostSchema = new mongoose.Schema({
   title: String,
-  description: String,
+  subtitle: String,
   content: String,
   username: String,
   // objectで指定することでdefault値の設定が可能、type keyでtype指定が必須
