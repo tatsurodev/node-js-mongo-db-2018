33d14a068083aa40d6d4bff2cd4bd2b342e203e9
Tatsuro <contacts@tatsuro.dev>
2020-10-17 18:04:31 +0900

    passwordをhash化するbcrypt install, mongoose model hookでuser保存時にpasswordをhash化 40

--- node-js-blog/database/models/User.js
+++ node-js-blog/database/models/User.js
@@ -1,9 +1,20 @@
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
 })
 
-module.exports = mongoose.model('User', userSchema)
+// mongoose model hook, save時に行う処理をhook
+UserSchema.pre('save', function (next) {
+  const user = this
+  // hash(data, salt, callback(error, encryptedData)), saltとは暗号化の際の強度、大きいと処理に時間がかかる
+  bcrypt.hash(user.password, 10, function (error, encrypted) {
+    user.password = encrypted
+    next()
+  })
+})
+
+module.exports = mongoose.model('User', UserSchema)
