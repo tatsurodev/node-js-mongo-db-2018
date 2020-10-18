371e4aa1b8628d5538e7d3e255562e5496f52e2c
Tatsuro <contacts@tatsuro.dev>
2020-10-19 00:29:38 +0900

    wysiwyg editorをtextareaに追加 57
    wysiwygはsummernoteを使用

--- node-js-blog/middleware/storePost.js
+++ node-js-blog/middleware/storePost.js
@@ -1,7 +1,7 @@
 // validation middlewareの作成
 module.exports = (req, res, next) => {
   // console.log(req.files)
-  if (!req.files || !req.body.username || !req.body.title || !req.body.subtitle || !req.body.content) {
+  if (!req.files || !req.body.title || !req.body.subtitle || !req.body.content) {
     return res.redirect('/posts/new')
   }
   // nextをcallしないとresponseがhangingになるので注意
